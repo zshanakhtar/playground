2722cd8fc5e929204a0f450e0665c61fc452638d
Zeeshan Akhtar <zshanakhtar@outlook.com>
2019-11-12 18:52:07 +0530

    added mongoose connection

--- jwtauthnode-3/index.js
+++ jwtauthnode-3/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+//Connect DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true },
+    () => console.log('connected to DB')
+);
 
 //Import Routes
 const authRoute = require('./routes/auth')
