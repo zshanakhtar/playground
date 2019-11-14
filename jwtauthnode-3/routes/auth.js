1a224c2f39de08d6790574406a04c8296c6f69be
Zeeshan Akhtar <zshanakhtar@outlook.com>
2019-11-15 01:34:20 +0530

    hashing passwords

--- jwtauthnode-3/routes/auth.js
+++ jwtauthnode-3/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const {registerValidation} = require('../validation')
-
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req,res) => {
     
@@ -14,16 +14,20 @@ router.post('/register', async (req,res) => {
     const emailExists = await User.findOne({email: req.body.email});
     if (emailExists)
         return res.status(400).send('Email already exists');
+    
+    //hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     //create new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user : savedUser._id});
     }catch(err){
         res.status(400).send(err);
     }
